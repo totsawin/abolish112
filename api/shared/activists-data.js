d53108001fb8a6d2d34a3f4031c25fd8a206b5d5
windisnothere <totsawin.jangprasert@exxonmobil.com>
2021-09-02 13:36:56 +0700

    migrate data to cosmosDB

--- api/shared/activists-data.js
+++ api/shared/activists-data.js
@@ -1,235 +1,229 @@
 const data = {
     activists: [
 		{
-			name: `จตุภัทร์ บุญภัทรรักษา "ไผ่ดาวดิน"`,
-			nickname: 'pai',
-			detainedDuration: [
+			"name": "จตุภัทร์ บุญภัทรรักษา 'ไผ่ดาวดิน'",
+			"nickname": "pai",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 8 },
-					releasedDate: { year: 2021, month: 4, day: 23 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 8 },
+					"releasedDate": { "year": 2021, "month": 4, "day": 23 }
 				},
 				{
-					detainedDate: { year: 2021, month: 8, day: 11 },
+					"detainedDate": { "year": 2021, "month": 8, "day": 11 }
 				}
 			]
 		},
 		{
-			name: `พริษฐ์ ชิวารักษ์ "เพนกวิน"`,
-			nickname: 'penguin',
-			detainedDuration: [
+			"name": "พริษฐ์ ชิวารักษ์ 'เพนกวิน'",
+			"nickname": "penguin",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 9 },
-					releasedDate: { year: 2021, month: 5, day: 11 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 9 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 11 }
 				},
 				{
-					detainedDate: { year: 2021, month: 8, day: 9 },
+					"detainedDate": { "year": 2021, "month": 8, "day": 9 }
 				}
 			]
 		},
 		{
-			name: `ภาณุพงศ์ จาดนอก "ไมค์"`,
-			nickname: 'mike',
-			detainedDuration: [
+			"name": "ภาณุพงศ์ จาดนอก 'ไมค์'",
+			"nickname": "mike",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 8 },
-					releasedDate: { year: 2021, month: 6, day: 1 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 8 },
+					"releasedDate": { "year": 2021, "month": 6, "day": 1 }
 				},
 				{
-					detainedDate: { year: 2021, month: 8, day: 9 },
+					"detainedDate": { "year": 2021, "month": 8, "day": 9 }
 				}
 			]
 		},
 		{
-			name: `สมยศ พฤกษาเกษมสุข`,
-			nickname: 'somyot',
-			detainedDuration: [
+			"name": "สมยศ พฤกษาเกษมสุข",
+			"nickname": "somyot",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 9 },
-					releasedDate: { year: 2021, month: 4, day: 23 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 9 },
+					"releasedDate": { "year": 2021, "month": 4, "day": 23 }
 				}
 			]
 		},
 		{
-			name: `อานนท์ นำภา`,
-			nickname: 'arnon',
-			detainedDuration: [
+			"name": "อานนท์ นำภา",
+			"nickname": "arnon",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 9 },
-					releasedDate: { year: 2021, month: 6, day: 1 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 9 },
+					"releasedDate": { "year": 2021, "month": 6, "day": 1 }
 				},
 				{
-					detainedDate: { year: 2021, month: 8, day: 11 },
+					"detainedDate": { "year": 2021, "month": 8, "day": 11 }
 				}
 			]
 		},
 		{
-			name: `ปติวัฒน์ สาหร่ายแย้ม "หมอลำแบงค์"`,
-			nickname: 'bank',
-			detainedDuration: [
+			"name": "ปติวัฒน์ สาหร่ายแย้ม 'หมอลำแบงค์'",
+			"nickname": "bank",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 9 },
-					releasedDate: { year: 2021, month: 4, day: 9 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 9 },
+					"releasedDate": { "year": 2021, "month": 4, "day": 9 }
 				}
 			]
 		},
 		{
-			name: `ปนัสยา สิทธิจิรวัฒนกุล "รุ้ง"`,
-			nickname: 'rung',
-			detainedDuration: [
+			"name": "ปนัสยา สิทธิจิรวัฒนกุล 'รุ้ง'",
+			"nickname": "rung",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 8 },
-					releasedDate: { year: 2021, month: 5, day: 6 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 8 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 6 }
 				}
 			]
 		},
 		{
-			name: `อัญชัน (สงวนนามสกุล)`,
-			nickname: 'anchan',
-			detainedDuration: [
+			"name": "อัญชัน (สงวนนามสกุล)",
+			"nickname": "anchan",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 1, day: 19 },
+					"detainedDate": { "year": 2021, "month": 1, "day": 19 }
 				}
 			]
 		},
 		{
-			name: `ไชยอมร แก้ววิบูลย์พันธุ์ "แอมมี่ เดอะ บอตทอมบลูส์"`,
-			nickname: 'ammy',
-			detainedDuration: [
+			"name": "ไชยอมร แก้ววิบูลย์พันธุ์ 'แอมมี่ เดอะ บอตทอมบลูส์'",
+			"nickname": "ammy",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 4 },
-					releasedDate: { year: 2021, month: 5, day: 11 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 4 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 11 }
 				}
 			]
 		},
 		{
-			name: `ปริญญา ชีวินกุลปฐม "พอร์ท วงไฟเย็น"`,
-			nickname: 'port',
-			detainedDuration: [
+			"name": "ปริญญา ชีวินกุลปฐม 'พอร์ท วงไฟเย็น'",
+			"nickname": "port",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 6 },
-					releasedDate: { year: 2021, month: 5, day: 12 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 6 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 12 }
 				}
 			]
 		},
 		{
-			name: `ปิยรัฐ จงเทพ “โตโต้”`,
-			nickname: 'toto',
-			detainedDuration: [
+			"name": "ปิยรัฐ จงเทพ 'โตโต้'",
+			"nickname": "toto",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 6 },
-					releasedDate: { year: 2021, month: 5, day: 5 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 6 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 5 }
 				}
 			]
 		},
 		{
-			name: `พรหมศร วีระธรรมจารี "ฟ้า"`,
-			nickname: 'fah',
-			detainedDuration: [
+			"name": "พรหมศร วีระธรรมจารี 'ฟ้า'",
+			"nickname": "fah",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 17 },
-					releasedDate: { year: 2021, month: 5, day: 10 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 17 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 10 }
 				},
 				{
-					detainedDate: { year: 2021, month: 8, day: 9 },
+					"detainedDate": { "year": 2021, "month": 8, "day": 9 }
 				}
 			]
 		},
 		{
-			name: `ชูเกียรติ แสงวงค์ "จัสติน"`,
-			nickname: 'justin',
-			detainedDuration: [
+			"name": "ชูเกียรติ แสงวงค์ 'จัสติน'",
+			"nickname": "justin",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 23 },
-					releasedDate: { year: 2021, month: 6, day: 2 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 23 },
+					"releasedDate": { "year": 2021, "month": 6, "day": 2 }
 				}
 			]
 		},
 		{
-			name: `ธวัช สุขประเสริฐ`,
-			nickname: 'tawat',
-			detainedDuration: [
+			"name": "ธวัช สุขประเสริฐ",
+			"nickname": "tawat",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 24 },
-					releasedDate: { year: 2021, month: 5, day: 18 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 24 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 18 }
 				}
 			]
 		},
 		{
-			name: `ศักดิ์ชัย ตั้งจิตสดุดี`,
-			nickname: 'sakchai',
-			detainedDuration: [
+			"name": "ศักดิ์ชัย ตั้งจิตสดุดี",
+			"nickname": "sakchai",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 24 },
-					releasedDate: { year: 2021, month: 5, day: 13 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 24 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 13 }
 				}
 			]
 		},
 		{
-			name: `ฉลวย เอกศักดิ์`,
-			nickname: 'chaluay',
-			detainedDuration: [
+			"name": "ฉลวย เอกศักดิ์",
+			"nickname": "chaluay",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 24 },
-					releasedDate: { year: 2021, month: 5, day: 13 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 24 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 13 }
 				}
 			]
 		},
 		{
-			name: `ณัฐนนท์ ไชยมหาบุตร`,
-			nickname: 'frank',
-			detainedDuration: [
+			"name": "ณัฐนนท์ ไชยมหาบุตร",
+			"nickname": "frank",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 2, day: 24 },
-					releasedDate: { year: 2021, month: 5, day: 18 },
+					"detainedDate": { "year": 2021, "month": 2, "day": 24 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 18 }
 				}
 			]
 		},
 		{
-			name: `สิรภพ พุ่มพึ่ง`,
-			nickname: 'kanoon',
-			detainedDuration: [
+			"name": "สิรภพ พุ่มพึ่ง",
+			"nickname": "kanoon",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 5, day: 6 },
-					releasedDate: { year: 2021, month: 5, day: 9 },
+					"detainedDate": { "year": 2021, "month": 5, "day": 6 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 9 }
 				}
 			]
 		},
 		{
-			name: `หทัยรัตน์ แก้วสีคราม`,
-			nickname: 'ploy',
-			detainedDuration: [
+			"name": "หทัยรัตน์ แก้วสีคราม",
+			"nickname": "ploy",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 5, day: 2 },
-					releasedDate: { year: 2021, month: 5, day: 10 },
+					"detainedDate": { "year": 2021, "month": 5, "day": 2 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 10 }
 				}
 			]
 		},
 		{
-			name: `พรชัย`,
-			nickname: 'pornchai',
-			detainedDuration: [
+			"name": "พรชัย",
+			"nickname": "pornchai",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 3, day: 11 },
-					releasedDate: { year: 2021, month: 4, day: 22 },
+					"detainedDate": { "year": 2021, "month": 3, "day": 11 },
+					"releasedDate": { "year": 2021, "month": 4, "day": 22 }
 				}
 			]
 			
 		},
 		{
-			name: `วรรณวลี ธรรมสัตยา`,
-			nickname: 'tee',
-			detainedDuration: [
+			"name": "วรรณวลี ธรรมสัตยา",
+			"nickname": "tee",
+			"detainedDuration": [
 				{
-					detainedDate: { year: 2021, month: 4, day: 27 },
-					releasedDate: { year: 2021, month: 5, day: 7 },
+					"detainedDate": { "year": 2021, "month": 4, "day": 27 },
+					"releasedDate": { "year": 2021, "month": 5, "day": 7 }
 				}
 			]
 		},
 	]
 };
-
-const getActivists = () => {
-    return data.activists;
-  };
-  
-module.exports = { getActivists };
